eb0437b07e60908763addeeead765f6292cf0e27
bhawana59 <gahlotbhawana9@gmail.com>
2026-07-28 15:47:40 +0530

    Updated documentation

--- app.py
+++ app.py
@@ -1,5 +1,2 @@
 def welcome(name):
-    return f"Hello {name}, Jenkins CI Pipeline Working!"
-
-if __name__ == "__main__":
-    print(welcome("Developer"))
+    return f"Hello {name}, CI/CD is working!"
